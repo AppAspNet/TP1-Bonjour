f466924dbfc4961b56823bd2fc0c69223905e708
essarraj <essarraj.fouad@gmail.com>
2015-02-27 10:32:07 +0000

    Page Bonjour
    Page qui affiche le message bonjour

---
+++ Bonjour/PageBonjour.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bonjour
+{
+    public partial class PageBonjour : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+        }
+
+        protected void BtBonjour_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "Bonjour : " + TextBox1.Text;
+        }
+    }
+}
